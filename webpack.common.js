45697991771f319b6e35289146cdac1b5b2ffb40
RMjessica <88908436+RMjessica@users.noreply.github.com>
2022-10-13 21:41:11 +0200

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: [
+    './src/front/js/index.js'
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader']
+        },
+        {
+          test: /\.(css|scss)$/, use: [{
+              loader: "style-loader" 
+          }, {
+              loader: "css-loader" 
+          }]
+        },
+        {
+          test: /\.(png|svg|jpg|gif|jpeg|webp)$/, use: {
+            loader: 'file-loader',
+            options: { name: '[name].[ext]' }
+          }
+        }, 
+        { test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/, use: ['file-loader'] }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+        favicon: 'j.png',
+        template: 'template.html'
+    }),
+    new Dotenv({ safe: true, systemvars: true })
+  ]
+};
